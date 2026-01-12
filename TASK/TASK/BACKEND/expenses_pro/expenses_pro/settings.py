70685a7d8fe1f35be29ef9955eadc06048351f4a
Ishika Sakhare <81871086+Ishika2418@users.noreply.github.com>
2026-01-12 11:53:06 +0530

    Update settings.py

--- TASK/TASK/BACKEND/expenses_pro/expenses_pro/settings.py
+++ TASK/TASK/BACKEND/expenses_pro/expenses_pro/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-(2i^hy0wb!bral@&gdaixls=vf9!2(mpzx3t&z*tdqz-4*=-pv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ".onrender.com"]
+
 
 
 # Application definition
