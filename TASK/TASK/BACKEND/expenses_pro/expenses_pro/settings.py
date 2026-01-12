6b9804799d0c55638d925b1638a2047b2fc5dd88
Ishika Sakhare <81871086+Ishika2418@users.noreply.github.com>
2026-01-12 11:55:44 +0530

    Update settings for production deployment
    Set DEBUG to False for production and configure STATIC_ROOT.

--- TASK/TASK/BACKEND/expenses_pro/expenses_pro/settings.py
+++ TASK/TASK/BACKEND/expenses_pro/expenses_pro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(2i^hy0wb!bral@&gdaixls=vf9!2(mpzx3t&z*tdqz-4*=-pv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", ".onrender.com"]
 
@@ -124,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
